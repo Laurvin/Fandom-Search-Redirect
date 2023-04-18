9073d16d455e90fe3fb520d130e98daa96cc1862
Laurvin <github@dbbv.com>
2023-04-18 14:59:11 +0200

    Update Fandom_Search_Redirect.user.js

--- Fandom_Search_Redirect.user.js
+++ Fandom_Search_Redirect.user.js
@@ -3,8 +3,9 @@
 // @namespace Fandom Search Redirect
 // @author Laurvin
 // @description Redirects the search to the correct page if there is a complete match
-// @version 1.01
+// @version 1.1
 // @icon http://i.imgur.com/XYzKXzK.png
+// @updateURL https://github.com/Laurvin/Fandom-Search-Redirect/raw/main/Fandom_Search_Redirect.user.js
 // @match https://*.fandom.com/wiki/Special:Search?*
 // @grant none
 // @run-at document-idle
